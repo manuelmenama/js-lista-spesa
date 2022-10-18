a785ba1d47bf2f16c8ca512e50a0bfee5a932982
manuelmenama <manuel.mena090589@gmail.com>
2022-10-18 15:16:49 +0200

    first but later

---
+++ js/script.js
@@ -0,0 +1,22 @@
+const shopList = [
+  'carbone per il gas e la siderurgia 6000000 t',
+  'acciaio 2000000 t',
+  'petrolio 7000000 t',
+  'legno 1000000 t',
+  'rame 150000 t',
+  'nitrato di sodio 220000 t',
+  'sali potassici 70000 t',
+  'colofonia 25000 t',
+  'gomma 22000 t',
+  'toluolo 18000 t',
+  'piombo 10000 t',
+  'stagno 7000 t',
+  'nikelio 5000 t',
+  'molibdeno 600 t',
+  'tungsteno 600 t',
+  'zirconio 20 t',
+  'titanio 400 t',
+];
+
+const lista = document.querySelector('ul');
+
